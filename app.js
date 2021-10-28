f510697660377237250cdb94ac31807bf7847275
lkm779 <luiskm7796@gmail.com>
2021-10-28 21:43:09 +0100

    updated app title

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const require= mongoose('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb+srv://lkm779:'+ process.env.MONGO_PASSWORD + ' @cluster0.hw5pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',) 
+{
+  useMongoClient: true}
+
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
 
